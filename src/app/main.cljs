6fcb6dae7a39c8a07bfc03cdf583f42d6350286d
Josh Brown <brown.joshua49@aol.com>
2018-01-02 06:19:49 -0800

    another shadow-cljs setup

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,15 +1,19 @@
-(ns app.main
-  (:require (app.lib :as lib)))
+(ns app.main)
 
-(def a 1)
+(defn start []
+  ;; start is called by init and after code reloading finishes
+  ;; this is controlled by the :after-load in the config
+  (js/console.log "start"))
 
-(defonce b 2)
+(defn ^:export init []
+  ;; init is called ONCE when the page loads
+  ;; this is called in the index.html and must be exported
+  ;; so it is available even in :advanced release builds
+  (js/console.log "init")
+  (start))
 
-(defn main! []
-  (println "[main]: loading"))
+(defn stop []
+  ;; stop is called before any code is reloaded
+  ;; this is controlled by :before-load in the config
+  (js/console.log "stop"))
 
-(defn reload! []
-  (println "[main] reloaded lib:" lib/c lib/d)
-  (println "[main] reloaded:" a b))
-
-(main!)
